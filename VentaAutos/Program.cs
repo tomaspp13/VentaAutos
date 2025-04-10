3684c2d60c6c927a99682d32ed3a029d49368e44
Tomas Agustin Silva Nuñez <tomassilvanunez@gmail.com>
2025-04-10 12:27:30 -0300

    Fix: cierre de paréntesis en UseNpgsql

--- VentaAutos/Program.cs
+++ VentaAutos/Program.cs
@@ -1,25 +1,23 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using NuGet.Packaging;
-using VentaAutos.Models; // Asegúrate de que este namespace coincide con el de AppDbContext
+using VentaAutos.Models; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar la configuración del DbContext con SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Agregar servicios de autorización
 builder.Services.AddAuthorization();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  
     app.UseHsts();
 }
 
