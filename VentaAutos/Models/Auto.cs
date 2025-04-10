0d1bd313bf495e2e34af355ea814aa9f7a81e22a
Tomas Agustin Silva Nuñez <tomassilvanunez@gmail.com>
2025-04-10 16:55:35 -0300

    Eliminados los ? de las propiedades string en el modelo Auto

--- VentaAutos/Models/Auto.cs
+++ VentaAutos/Models/Auto.cs
@@ -8,11 +8,11 @@ namespace VentaAutos.Models
     public class Auto
     {
         public int Id { get; set; }
-        public string? Marca { get; set; }
-        public string? Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
 
         [Display(Name = "Año")]
-        public string? Anio { get; set; }
+        public string Anio { get; set; }
         
         [Precision(18, 2)]
         public decimal Precio { get; set; }
@@ -20,19 +20,19 @@ namespace VentaAutos.Models
         [Display(Name = "Kilometro")]
         public int? Km { get; set; }
 
-        public string? Color { get; set; }
+        public string Color { get; set; }
 
-        public string? Detalles { get; set; }
+        public string Detalles { get; set; }
 
-        public string? Motor { get; set; }
+        public string Motor { get; set; }
 
-        public string? Transmision { get; set; }
+        public string Transmision { get; set; }
 
-        public string? Capacidad_de_carga { get;set; }
+        public string Capacidad_de_carga { get;set; }
 
-        public string? Seguridad { get;set; }
+        public string Seguridad { get;set; }
 
-        public string? Conbustible { get; set; }
+        public string Conbustible { get; set; }
 
         public virtual ICollection<Imagen> Imagenes { get; set; } = new List<Imagen>();
 
@@ -42,7 +42,7 @@ namespace VentaAutos.Models
     {
 
         public int Id { get; set; }
-        public string? ImagenUrl { get; set; }
+        public string ImagenUrl { get; set; }
 
         public int IdAuto { get; set; }
 
